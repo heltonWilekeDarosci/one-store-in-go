8d4499b1339fb87c5b64bd1ae35cc9e5950abb82
helton <heltoloco@hotmail.com>
2022-11-04 11:34:18 -0300

    feat(store): create db and connect to it through func

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
+
+func connectDb() *sql.DB {
+	connection := "user=store dbname=one_store password=he20580020he host=localhost sslmode=disable"
+	db, err := sql.Open("postgres", connection)
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
 
 type Product struct {
 	Name        string
@@ -15,6 +27,8 @@ type Product struct {
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
+	db := connectDb()
+	defer db.Close()
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":8000", nil)
 }
